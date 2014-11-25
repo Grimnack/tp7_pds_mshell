e749c2c28817a04af3117d99ba99a8608c992e94
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-25 09:43:04 +0100

    pipe a 2 cmd

--- pipe.c
+++ pipe.c
@@ -6,16 +6,43 @@
    
 */
 
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "pipe.h"
+#include <assert.h>
+#include <unistd.h>
 
 
 void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
-
-
-  printf("Not implemented yet\n");
-
-  
-
+  int fds[2];
+  pipe(fds);
+  /*1er Fork*/
+  switch(fork()) 
+    {
+    case -1:
+      exit(EXIT_FAILURE);
+    case 0:
+      /*2e fork*/
+      switch(fork()) 
+	{
+	case -1: /*erreur*/
+	  exit(EXIT_FAILURE);
+	case 0: /*1er fils*/
+	  dup2(fds[1], STDOUT_FILENO);
+	  close(fds[0]);
+	  close(fds[1]);
+	  execvp(cmds[0][0], cmds[0]);
+	  break;
+	default: /*2e fils*/
+	  dup2(fds[0], STDIN_FILENO);
+	  close(fds[0]);
+	  close(fds[1]);
+	  execvp(cmds[1][0], cmds[1]);
+	}
+      break;
+    default:;
+    }
+  close(fds[0]);
+  close(fds[1]);
   return;
 }
