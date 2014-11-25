33cf6a77d47dc2b4c4ccbb7e19b74341cdf830cf
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-25 10:02:27 +0100

    ajout bg

--- pipe.c
+++ pipe.c
@@ -11,11 +11,13 @@
 #include "pipe.h"
 #include <assert.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
 
 void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
-  int fds[2];
-  pipe(fds);
+  int fds[MAXCMDS][2];
+  int i;
+  printf("%d commandes\n", nbcmd);
+  pipe(fds[0]);
   /*1er Fork*/
   switch(fork()) 
     {
@@ -27,22 +29,26 @@ void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
 	{
 	case -1: /*erreur*/
 	  exit(EXIT_FAILURE);
-	case 0: /*1er fils*/
-	  dup2(fds[1], STDOUT_FILENO);
-	  close(fds[0]);
-	  close(fds[1]);
+	case 0:	  
+	  /*1er fils*/
+	  dup2(fds[0][1], STDOUT_FILENO);
+	  close(fds[0][0]);
+	  close(fds[0][1]);
 	  execvp(cmds[0][0], cmds[0]);
 	  break;
-	default: /*2e fils*/
-	  dup2(fds[0], STDIN_FILENO);
-	  close(fds[0]);
-	  close(fds[1]);
-	  execvp(cmds[1][0], cmds[1]);
+	default:/*2e fils*/
+	  dup2(fds[0][0], STDIN_FILENO);
+	  close(fds[0][0]);
+	  close(fds[0][1]);
+	  execvp(cmds[1][0], cmds[1]);;
 	}
       break;
-    default:;
+    default: ;
     }
-  close(fds[0]);
-  close(fds[1]);
+  close(fds[0][0]);
+  close(fds[0][1]);
+  if(!bg)
+    for(i = 0; i < nbcmd; i++)
+      wait(0);
   return;
 }
