9ff36a07ad98b2e1d37b8f35ee552c90618cbcab
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-26 22:16:21 +0100

    it works !

--- pipe.c
+++ pipe.c
@@ -15,40 +15,74 @@
 
 void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
   int fds[MAXCMDS][2];
-  int i;
+  int i, j;
   printf("%d commandes\n", nbcmd);
   pipe(fds[0]);
+
   /*1er Fork*/
+  printf("%de fork (premier)\n", 0);
   switch(fork()) 
     {
     case -1:
       exit(EXIT_FAILURE);
+    case 0:	
+      dup2(fds[0][1], STDOUT_FILENO);
+      close(fds[0][0]);
+      close(fds[0][1]);
+      execvp(cmds[0][0], cmds[0]);
+      break;
+    }
+
+  for(i = 1; i < nbcmd-1; i++)
+    {
+      pipe(fds[i]);
+      printf("%de fork\n", i);
+      /*nbcmds Fork*/
+      switch(fork())
+	{
+	case -1:
+	  exit(EXIT_FAILURE);
+	case 0:	
+	  dup2(fds[i-1][0], STDIN_FILENO);
+	  dup2(fds[i][1], STDOUT_FILENO);
+
+	  for(j = 0; j <= i; j++)
+	    {
+	      close(fds[j][0]);
+	      close(fds[j][1]);
+	    }
+	  execvp(cmds[i][0], cmds[i]);
+	  break;
+	}
+    }
+  /*Dernier Fork*/
+  printf("%de fork (dernier)\n", i);
+  switch(fork()) 
+    {
+    case -1: /*erreur*/
+      exit(EXIT_FAILURE);
     case 0:
       /*2e fork*/
-      switch(fork()) 
+      dup2(fds[i-1][0], STDIN_FILENO);
+      for(j = 0; j < i; j++)
 	{
-	case -1: /*erreur*/
-	  exit(EXIT_FAILURE);
-	case 0:	  
-	  /*1er fils*/
-	  dup2(fds[0][1], STDOUT_FILENO);
-	  close(fds[0][0]);
-	  close(fds[0][1]);
-	  execvp(cmds[0][0], cmds[0]);
-	  break;
-	default:/*2e fils*/
-	  dup2(fds[0][0], STDIN_FILENO);
-	  close(fds[0][0]);
-	  close(fds[0][1]);
-	  execvp(cmds[1][0], cmds[1]);;
+	  close(fds[j][0]);
+	  close(fds[j][1]);
 	}
+      execvp(cmds[i][0], cmds[i]);
       break;
-    default: ;
     }
-  close(fds[0][0]);
-  close(fds[0][1]);
+
+  /* Fermeture des fds */
+  for(j = 0; j < i; j++)
+    {
+      close(fds[j][0]);
+      close(fds[j][1]);
+    }
+
+  /* Wait si bg est false*/
   if(!bg)
     for(i = 0; i < nbcmd; i++)
-      wait(0);
-  return;
+    wait(0);
+      return;
 }
